01ae88e6a2b0490b4c5ec28de0fd8e7bad817cb0
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-27 15:27:37 +0530

    comits

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,11 +1,13 @@
+
+import { FETCH_ALL, CREATE, UPDATE, DELETE} from '../constants/actionTypes';
 import * as api from '../api';
-import { FETCH_ALL, CREATE, UPDATE } from '../constants/actionTypes';
+
 //Actions Creators
 export const getPosts = () => async(dispatch)=> {
     try {
         const {data} = await api.fetchPosts();
 
-        dispatch({type: 'FETCH_ALL', payload: data});
+        dispatch({type: FETCH_ALL, payload: data});
     } catch (error) {
         console.log(error.message);
     }
@@ -16,7 +18,7 @@ export const createPost =(post)=> async(dispatch) =>{
     try {
         const {data} = await api.createPost(post);               //making an API Request to our backend server for post api requests
         
-        dispatch({type: 'CREATE', payload: data});
+        dispatch({type: CREATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -26,7 +28,7 @@ export const createPost =(post)=> async(dispatch) =>{
 export const updatePost = (id, post)=> async(dispatch)=>{
     try {
         const {data} = await api.updatePost(id, post);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -36,7 +38,7 @@ export const updatePost = (id, post)=> async(dispatch)=>{
 export const deletePost =(id)=>async(dispatch)=>{
     try {
         await api.deletePost(id);
-        dispatch({type: 'DELETE', payload: id});
+        dispatch({type: DELETE, payload: id});
     } catch (error) {
         console.log(error);
     }
@@ -45,7 +47,7 @@ export const deletePost =(id)=>async(dispatch)=>{
 export const likePost = (id)=> async(dispatch)=>{
     try {
         const {data} = await api.likePost(id);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     } catch (error) {
         console.log(error);
     }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
@@ -17,11 +19,11 @@ app.use('/posts', postRoutes);
 //connecting app to database(mongoDB)
 // const CONNECTION_URL = 'mongodb+srv://aishwarya-singh04:@Me_aish1@cluster0.xrzt5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 //const CONNECTION_URL='mongodb+srv://aishwarya-singh04:aishwarya123@cluster0.xrzt5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const CONNECTION_URL = 'mongodb+srv://simran_maurya:Simran123@cluster0.cadsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://simran_maurya:Simran123@cluster0.cadsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 //we use mongoose to connect to outr database
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port : ${PORT}`)))
     .catch((error)=> console.log(error.message));
 
