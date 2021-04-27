fd4de7b82f22188adf05cda4cfcaf6ca02e80520
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-27 14:29:58 +0530

    add on

--- client/src/App.js
+++ client/src/App.js
@@ -20,12 +20,12 @@ const App =()=>{
     return(
        <Container maxidth="lg">
            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60"/>
+                <Typography className={classes.heading} variant="h3" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="50"/>
            </AppBar>
            <Grow in>
                <Container>
-                   <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                   <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
                        <Grid items xs={12} sm={7}>
                             <Posts setCurrentId= {setCurrentId}/>
                        </Grid>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,7 +8,7 @@ export const getPosts = async(req,res)=>{
         const postMessages = await PostMessage.find();
         
         console.log(postMessages);
-
+        
         res.status(200).json(postMessages);
     }
     catch(error){
