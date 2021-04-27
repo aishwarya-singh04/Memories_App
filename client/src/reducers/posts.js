3aa4e4d8e0f5c127f9e432c578e5eef22f7fb3b1
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-27 13:43:07 +0530

    additional features

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -6,7 +6,7 @@ export default (posts=[], action) => {
         case 'LIKE':
             return posts.map((post)=> post._id===action.payload._id ? action.payload : post);
         case 'DELETE':
-            return posts.filter((post)=> post._id!=action.payload);
+            return posts.filter((post)=> post._id!==action.payload);
         case 'UPDATE':
             return posts.map((post)=> post._id===action.payload._id ? action.payload : post);
         case 'FETCH_ALL':
