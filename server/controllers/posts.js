56662983187835bc533eb9292ff44e77d8d8793f
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-27 12:49:08 +0530

    deletion

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -44,7 +44,7 @@ export const updatePost=async(req,res)=>{
 
 export const deletePost = async(req,res)=>{
     const {id} = req.params;
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
         await PostMessage.findByIdAndDelete(id);
 
