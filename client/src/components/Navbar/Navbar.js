1ff763272e5b6f0a48487f01899c30ef26d201d5
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-29 19:52:33 +0530

    name revised

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -100,8 +100,8 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <Typography component={Link} to="/" className={classes.heading} variant="h3" align="center">Retrospect</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="50" />
       </div>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
